9736779e1d62b162bc607a1424d21b4a7750b894
tejayy <tbj456@gmail.com>
2025-03-14 21:21:42 +0530

    Added education and exp component

--- src/components/EducationAndExperience.jsx
+++ src/components/EducationAndExperience.jsx
@@ -1,3 +1,7 @@
+import {motion} from "framer-motion";
+import {experience, education} from "./data/config";
+
+const SectionItem = ({title, subtitle, year, details, icon}) => {};
 export default function EducationAndExperience() {
   return <div>EducationAndExperience</div>;
 }
